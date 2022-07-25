beb714b43447a7a7e63cd73728e2c32b0dd5d9c4
starbvuks <starbvuks@gmail.com>
2022-07-25 12:52:42 +0530

    projoy init

--- ecomm-next/pages/index.js
+++ ecomm-next/pages/index.js
@@ -6,10 +6,13 @@ export default function Home({ posts }) {
       {posts &&
         posts.data.map((post) => (
           <div className={styles.blogs}>
-            <h2>{post.attributes.Title}</h2>
-            <h3 className={styles.author}>
-              {post.attributes.user.data.attributes.username}
-            </h3>
+            <img
+              style={{ maxWidth: "20%" }}
+              src={`http://localhost:1337${posts.data[0].attributes.image.data[0].attributes.url}`}
+            />
+            <h2>{post.attributes.title}</h2>
+            <h3 className={styles.author}>{post.attributes.price}</h3>
+            <p>{post.attributes.description}</p>
             <br></br>
           </div>
         ))}
@@ -18,11 +21,14 @@ export default function Home({ posts }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch("http://localhost:1337/api/posts?populate=*");
+  const res = await fetch("http://localhost:1337/api/products?populate=*");
+  // const res = await fetch("http://localhost:1337/api/posts?populate=*");
   const posts = await res.json();
 
+  console.log(posts.data[0].attributes.image.data[0].attributes.url);
+
   // posts.data.attributes to access data
-  console.log(posts.data[1].attributes.user.data.attributes.username);
+  // console.log(posts.data[1].attributes.user.data.attributes.username);
 
   return {
     props: { posts },
