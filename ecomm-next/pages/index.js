2e4993b92f3396bcc183f1e105b1a1a45b64e4fe
starbvuks <starbvuks@gmail.com>
2022-07-25 13:19:44 +0530

    image map fix

--- ecomm-next/pages/index.js
+++ ecomm-next/pages/index.js
@@ -6,14 +6,21 @@ export default function Home({ posts }) {
       {posts &&
         posts.data.map((post) => (
           <div className={styles.blogs}>
-            <img
-              style={{ maxWidth: "20%" }}
-              src={`http://localhost:1337${posts.data[0].attributes.image.data[0].attributes.url}`}
-            />
+            {post &&
+              post.attributes.image.data.map((pos) => (
+                <img
+                  style={{ maxWidth: "20%" }}
+                  src={`http://localhost:1337${pos.attributes.url}`}
+                />
+              ))}
+            {console.log(post.attributes.image.data)}
             <h2>{post.attributes.title}</h2>
             <h3 className={styles.author}>{post.attributes.price}</h3>
+            <h3 className={styles.author}>SKU: {post.attributes.SKU}</h3>
+            <h4 className={styles.author}>
+              Category: {post.attributes.category.data.attributes.title}
+            </h4>
             <p>{post.attributes.description}</p>
-            <br></br>
           </div>
         ))}
     </div>
@@ -25,7 +32,7 @@ export async function getStaticProps() {
   // const res = await fetch("http://localhost:1337/api/posts?populate=*");
   const posts = await res.json();
 
-  console.log(posts.data[0].attributes.image.data[0].attributes.url);
+  // console.log(posts.data[0].attributes.category.data.attributes.title);
 
   // posts.data.attributes to access data
   // console.log(posts.data[1].attributes.user.data.attributes.username);
