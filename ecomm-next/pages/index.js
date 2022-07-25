a4166ec8b402f8520c12ddd96e5c8e636d646e2a
starbvuks <starbvuks@gmail.com>
2022-07-25 16:12:55 +0530

    flavor variant init

--- ecomm-next/pages/index.js
+++ ecomm-next/pages/index.js
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
-export default function Home({ posts }) {
+export default function Home({ posts, flavors }) {
+  const [currFlavor, setCurrFlavor] = useState("Vanilla");
+
+  function handleSubmit(id) {
+    fetch(`http://localhost:1337/api/flavors/${id}`)
+      .then((data) => data.json())
+      .then((j) => setCurrFlavor(j))
+      .catch((error) => console.log(error));
+
+    console.log(currFlavor.data.attributes.title);
+  }
+
   return (
     <div className={styles.container}>
       {posts &&
@@ -13,14 +25,20 @@ export default function Home({ posts }) {
                   src={`http://localhost:1337${pos.attributes.url}`}
                 />
               ))}
-            {console.log(post.attributes.image.data)}
             <h2>{post.attributes.title}</h2>
+            <h2>Flavor: {currFlavor.data.attributes.title}</h2>
             <h3 className={styles.author}>{post.attributes.price}</h3>
             <h3 className={styles.author}>SKU: {post.attributes.SKU}</h3>
             <h4 className={styles.author}>
               Category: {post.attributes.category.data.attributes.title}
             </h4>
             <p>{post.attributes.description}</p>
+            {flavors &&
+              flavors.data.map((flavor) => (
+                <button onClick={() => handleSubmit(flavor.id)}>
+                  {flavor.attributes.title}
+                </button>
+              ))}
           </div>
         ))}
     </div>
@@ -29,8 +47,11 @@ export default function Home({ posts }) {
 
 export async function getStaticProps() {
   const res = await fetch("http://localhost:1337/api/products?populate=*");
+  const res2 = await fetch("http://localhost:1337/api/flavors?populate=*");
   // const res = await fetch("http://localhost:1337/api/posts?populate=*");
+
   const posts = await res.json();
+  const flavors = await res2.json();
 
   // console.log(posts.data[0].attributes.category.data.attributes.title);
 
@@ -38,6 +59,6 @@ export async function getStaticProps() {
   // console.log(posts.data[1].attributes.user.data.attributes.username);
 
   return {
-    props: { posts },
+    props: { posts, flavors },
   };
 }
