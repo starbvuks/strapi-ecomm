5a0c45f64e82dfdb40cd214c1f1388c6f47ed9c6
starbvuks <starbvuks@gmail.com>
2022-07-29 19:20:16 +0530

    cart page

---
+++ ecomm-next/components/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles/Cart.module.css";
+
+const Cart = () => {
+  return (
+    <div>
+      <h1 className={styles.header}>Cart</h1>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ ecomm-next/pages/cart.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Cart from "../components/Cart";
+import styles from "../styles/Cart.module.css";
+
+const cart = () => {
+  return (
+    <div>
+      <Cart />
+    </div>
+  );
+};
+
+export default cart;
